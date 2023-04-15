941568a25ce0941d4c9229a6a2a4fda00178b39d
tolis-0 <yotravik@gmail.com>
2023-04-15 18:14:19 +0300

    ANTE

---
+++ src/main/java/myy803/project/ThesisManagerApplication.java
@@ -0,0 +1,13 @@
+package myy803.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThesisManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThesisManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/myy803/project/ThesisManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package myy803.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThesisManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
