cf6bf2f5b05fdc5b4a428d60d36e156185918466
tolis-0 <yotravik@gmail.com>
2023-04-18 15:59:21 +0300

    added login html and controller

---
+++ src/main/java/myy803/project/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package myy803.project.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+		http.authorizeHttpRequests()
+		.antMatchers("/", "/login").permitAll();
+
+		return http.build();
+	}
+}
