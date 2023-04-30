2fca6ce3edb08fd3345740035434fa372efda357
Patroklos2000 <patroklospappas3@gmail.com>
2023-04-30 18:31:10 +0300

    added stuff

--- src/main/java/myy803/project/model/Professor.java
+++ src/main/java/myy803/project/model/Professor.java
@@ -6,6 +6,38 @@ import javax.persistence.Table;
 @Entity
 @Table(name="professors")
 public class Professor{
-
-
+	
+	private String full_name;
+	private String specialty;
+	
+	public Professor(String full_name, String specialty) {
+		this.full_name = full_name;
+		this.specialty = specialty;
+	}
+	
+	public Professor() {
+		
+	}
+	
+	public void setFull_name(String full_name) {
+		this.full_name = full_name;
+	}
+	
+	public void setSpecialty(String specialty) {
+		this.specialty = specialty;
+	}
+	
+	public String getFull_name() {
+		return this.full_name;
+	}
+	
+	public String getSpecialty() {
+		return this.specialty;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Professor [full_name=" + full_name + ", specialty=" + specialty + "]";
+	}
 }
--- src/main/java/myy803/project/model/Student.java
+++ src/main/java/myy803/project/model/Student.java
@@ -7,5 +7,56 @@ import javax.persistence.Table;
 @Table(name="students")
 public class Student{
 
-
+	private String full_name;
+	private int rem_courses;
+	private int year;
+	private float avg_grade;
+	
+	public Student(String full_name, int rem_courses, int year, float avg_grade) {
+		this.full_name = full_name;
+		this.rem_courses = rem_courses;
+		this.year = year;
+		this.avg_grade = avg_grade;
+	}
+	
+	public Student() {
+		
+	}
+	
+	public void setFull_name(String full_name) {
+		this.full_name = full_name;
+	}
+	
+	public void setRem_courses(int rem_courses) {
+		this.rem_courses = rem_courses;
+	}
+	
+	public void setYear(int year) {
+		this.year = year;
+	}
+	
+	public void setAvg_grade(float avg_grade) {
+		this.avg_grade = avg_grade;
+	}
+	
+	public String getFull_name() {
+		return this.full_name;
+	}
+	
+	public int getRem_courses() {
+		return this.rem_courses;
+	}
+	
+	public int getYear() {
+		return this.year;
+	}
+	
+	public float getAvg_grades() {
+		return this.avg_grade;
+	}
+	
+	@Override
+	public String toString() {
+		return "Student [full_name=" + full_name + ", rem_courses=" + rem_courses + ", year=" + year + ", avg_grade=" + avg_grade + "]";
+	}
 }
