4129693ea71b28dba2fe8e0fe39ba2c78551dcdf
tolis-0 <yotravik@gmail.com>
2023-05-02 21:27:10 +0300

    login and register progress

--- src/main/java/myy803/project/controller/AuthController.java
+++ src/main/java/myy803/project/controller/AuthController.java
@@ -2,10 +2,14 @@ package myy803.project.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import myy803.project.service.UserService;
+import myy803.project.model.User;
+
 
 @Controller
 public class AuthController {
@@ -14,23 +18,25 @@ public class AuthController {
     UserService userService;
 	
 	@GetMapping("/login")
-	public String loginPage() {
+	public String loginPage(Model model) {
+		model.addAttribute("user", new User());
 		return "login";
 	}
 	
 	@GetMapping("/register")
-	public String registerPage() {
+	public String registerPage(Model model) {
+		model.addAttribute("user", new User());
 		return "login";
 	}
 	
-	@PostMapping("login")
-	public String loginAttempt() {
+	@PostMapping("/post/login")
+	public String loginAttempt(@ModelAttribute("user") User user, Model model) {
 		return null; //TODO
 		
 	}
 	
-	@PostMapping("register")
-	public String registerAttempt() {
+	@PostMapping("/post/register")
+	public String registerAttempt(@ModelAttribute("user") User user, Model model) {
 		return null; //TODO
 	}
 }
