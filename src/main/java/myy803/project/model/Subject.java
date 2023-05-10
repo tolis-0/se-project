0de10a017acd692ef3609989adb4fdb5192e8e5b
tolis-0 <yotravik@gmail.com>
2023-05-10 19:47:33 +0300

    Toso apla...

--- src/main/java/myy803/project/model/Subject.java
+++ src/main/java/myy803/project/model/Subject.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -15,6 +17,7 @@ import javax.persistence.Table;
 public class Subject {
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name="id", nullable=false)
 	private int id;
 	
