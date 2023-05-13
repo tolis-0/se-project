f73d1cdcf1a85550fd248fb53aee53d82e8257b8
tolis-0 <yotravik@gmail.com>
2023-05-13 23:05:10 +0300

    updated chooseThesisAssignment

--- src/main/java/myy803/project/service/ThesisService.java
+++ src/main/java/myy803/project/service/ThesisService.java
@@ -1,12 +1,16 @@
 package myy803.project.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import myy803.project.dto.SelectApplicationDTO.SelectStrategy;
+import myy803.project.model.Application;
 import myy803.project.model.Thesis;
 
 @Service
 public interface ThesisService {
-	public void chooseThesisAssignment(int subjectId, int strategy);
+	public Thesis chooseThesisAssignment(List<Application> list, SelectStrategy strategy);
 	public Thesis saveThesis(Thesis thesis);
 	public float showTotalGrade(Thesis thesis);
 }
--- src/main/java/myy803/project/service/ThesisServiceImpl.java
+++ src/main/java/myy803/project/service/ThesisServiceImpl.java
@@ -1,53 +1,46 @@
 package myy803.project.service;
 
+import java.util.Comparator;
 import java.util.List;
 
 import java.util.Random;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import myy803.project.model.Application;
+
+import myy803.project.model.Student;
 import myy803.project.model.Thesis;
 import myy803.project.dao.ThesisDAO;
+import myy803.project.dto.SelectApplicationDTO.SelectStrategy;
+
 
 @Service
 public class ThesisServiceImpl implements ThesisService{
 	
 	@Autowired
-	private ApplicationService applicationService;
-	
-	@Autowired
 	private ThesisDAO thesisDAO;
 
-	public void chooseThesisAssignment(int subjectId, int strategy) {
-		List<Application> list = applicationService.getApplicationsBySubjectId(subjectId);
-		if (strategy == 1) {
-			Random rand = new Random();
-			int rand_int1 = rand.nextInt(list.size()-1);
-			list.get(rand_int1).getStudentId();
-			//create thesis
+	public Thesis chooseThesisAssignment(List<Application> list, SelectStrategy strategy) {
+		List<Student> students = list.stream().map(Application::getStudent).collect(Collectors.toList());
+		Student student;
+		switch (strategy) {
+			case RANDOM:
+				Random rand = new Random();
+				student = list.get(rand.nextInt(list.size())).getStudent();
+				break;
+			case AVERAGE_GRADE:
+				student = students.stream().max(Comparator.comparing(Student::getAvg_grades)).get();
+				break;
+			case REMAINING_COURSES:
+				student = students.stream().min(Comparator.comparing(Student::getRem_courses)).get();
+				break;
+			case MANUALLY:
+				return null;
 		}
-		if (strategy == 2) {
-			int student_id = list.get(0).getStudentId();
-			float best_avg_grades = list.get(0).getStudent().getAvg_grades();
-			for (int i = 1; i<list.size(); i++) {
-				if (best_avg_grades<list.get(i).getStudent().getAvg_grades()) {
-					best_avg_grades = list.get(i).getStudent().getAvg_grades();
-					student_id = list.get(i).getStudentId();
-				}
-			}
-		}
-		if (strategy == 3) {
-			int student_id = list.get(0).getStudentId();
-			int rem_courses = list.get(0).getStudent().getRem_courses();
-			for (int i = 1; i<list.size(); i++) {
-				if (rem_courses>list.get(i).getStudent().getRem_courses()) {
-					rem_courses = list.get(i).getStudent().getRem_courses();
-					student_id = list.get(i).getStudentId();
-				}
-			}
-		}
+		return null;
 	}
 	
 	@Override
