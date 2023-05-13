e78ff57f71d8ff21d582157089a5665c21887391
Patroklos2000 <patroklospappas3@gmail.com>
2023-05-13 21:21:54 +0300

    applications

--- src/main/java/myy803/project/service/ProfessorServiceImpl.java
+++ src/main/java/myy803/project/service/ProfessorServiceImpl.java
@@ -1,9 +1,14 @@
 package myy803.project.service;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Random;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import myy803.project.dao.ProfessorDAO;
+import myy803.project.model.Application;
 import myy803.project.model.Professor;
 
 @Service
@@ -14,6 +19,9 @@ public class ProfessorServiceImpl implements ProfessorService {
 	
 	@Autowired
 	private UserService userService;
+	
+	@Autowired
+	private ApplicationService applicationService;
 	
 	@Override
 	public Professor saveProfessor(Professor professor) {
@@ -27,5 +35,18 @@ public class ProfessorServiceImpl implements ProfessorService {
 		professor.setUser(userService.getUserById(professor.getId()));
 		return professor;
 	}
-
+	
+	public void chooseSubjectApplication(int subjectId, int strategy) {
+		List<Application> list = applicationService.getApplicationsBySubjectId(subjectId);
+		if (strategy == 1) {
+			Random rand = new Random();
+			int rand_int1 = rand.nextInt(list.size()-1);
+			list.get(rand_int1).getStudentId();
+			//create thesis
+		}
+		if (strategy == 2) {
+			int student_id = list.get(0).getStudentId();
+//			int best_avg_grades = 
+		}
+	}
 }
