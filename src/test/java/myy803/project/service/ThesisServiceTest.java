a8a57b735efcf18bdebfbd196a5add2087843a64
Patroklos2000 <patroklospappas3@gmail.com>
2023-05-20 01:07:21 +0300

    thesis test

--- src/test/java/myy803/project/service/ThesisServiceTest.java
+++ src/test/java/myy803/project/service/ThesisServiceTest.java
@@ -17,7 +17,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.TestPropertySource;
 
+import myy803.project.model.Application;
+import myy803.project.model.Professor;
+import myy803.project.model.Role;
+import myy803.project.model.Student;
 import myy803.project.model.Thesis;
+import myy803.project.model.User;
 
 @SpringBootTest
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
@@ -29,11 +34,19 @@ public class ThesisServiceTest {
 	@Autowired
 	ThesisService thesisService;
 	
+	@Autowired
+	StudentService studentService;
+	
+	@Autowired
+	UserService userService;
+	
 	@Test
 	@Order(1)
 	public void ThesisService_notNull() {
 		System.out.println("TEST 1");
 		Assertions.assertNotNull(thesisService);
+		Assertions.assertNotNull(studentService);
+		Assertions.assertNotNull(userService);
 		
 	}
 	
@@ -41,11 +54,50 @@ public class ThesisServiceTest {
 	@Order(2)
 	public void ThesisService_saveThesis() {
 		System.out.println("TEST 2");
+		
+		User user1 = userService.saveUser(new User("panana", "Password123", Role.STUDENT));
+		User user2 = userService.saveUser(new User("stefpap", "Password456", Role.STUDENT));
+		
+		Student stud1 = studentService.saveStudent(new Student(user1, "Panagiotis Anagnostou"));		
+		Student stud2 = studentService.saveStudent(new Student(user2, "Stefanos Pappas"));
+		
+		Thesis th1 = thesisService.saveThesis(new Thesis(3, stud1));
+		Thesis th2 = thesisService.saveThesis(new Thesis(6, stud2));
+		
+		Assertions.assertNotNull(th1);
+		Assertions.assertNotNull(th2);
+		
+		Assertions.assertEquals(th1.getId(), 3);
+		Assertions.assertEquals(th1.getStudent(), stud1);
+		Assertions.assertEquals(th2.getId(), 6);
+		Assertions.assertEquals(th2.getStudent(), stud2);
+
 	}
 	
 	@Test
-	@Order(2)
-	public void ThesisService_saveNewThesis() {
+	@Order(3)
+	public void ThesisService_getThesisById() {
 		System.out.println("TEST 3");
+		
+		User user1 = userService.saveUser(new User("panana", "Password123", Role.STUDENT));
+		User user2 = userService.saveUser(new User("stefpap", "Password456", Role.STUDENT));
+		
+		Student stud1 = studentService.saveStudent(new Student(user1, "Panagiotis Anagnostou"));		
+		Student stud2 = studentService.saveStudent(new Student(user2, "Stefanos Pappas"));
+		
+		thesisService.saveThesis(new Thesis(3, stud1));
+		thesisService.saveThesis(new Thesis(6, stud2));
+		
+		Thesis th1 = thesisService.getThesisById(3);
+		Thesis th2 = thesisService.getThesisById(6);
+		
+		Assertions.assertNotNull(th1);
+		Assertions.assertNotNull(th2);
+		
+		Assertions.assertEquals(th1.getId(), 3);
+		Assertions.assertEquals(th1.getStudent(), stud1);
+		Assertions.assertEquals(th2.getId(), 6);
+		Assertions.assertEquals(th2.getStudent(), stud2);
+		
 	}
 }
